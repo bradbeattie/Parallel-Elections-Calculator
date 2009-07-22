fb9cf5b8d61a50738d35ab47195ec43bf1f8360d
Brad Beattie <bradbeattie@gmail.com>
2009-07-22 08:31:50 -0700

    Addressing http://github.com/bradbeattie/Parallel-Elections-Calculator/issues/#issue/23

--- lib/Elections/RankedPairs.rb
+++ lib/Elections/RankedPairs.rb
@@ -12,15 +12,26 @@ module Elections
 			require 'rgl/adjacency'
 			lockedPairs = Array.new
 			results[:locking] = Array.new
-			results[:sortedStrongPairs].each do |v,p|
-				if RGL::DirectedAdjacencyGraph[*(lockedPairs+p).flatten].cycles.length == 0 
-					lockedPairs << p.collect { |c| c }
-					p.each { |p| results[:locking] << [p[0], p[1], "Kept"] }
-				else
-					p.each { |p| results[:locking] << [p[0], p[1], "Discarded"] }
-				end
-			end
+			results[:warnings] = Array.new
+			results[:sortedStrongPairs].each do |v,pairs|
 			
+				# Sort out which pairs cause cycles
+				cyclic_pairs = pairs.select { |p| RGL::DirectedAdjacencyGraph[*(lockedPairs+p).flatten].cycles.length > 0 }
+				pairs -= cyclic_pairs
+
+				# If we have to randomly select, throw up a warning
+				while RGL::DirectedAdjacencyGraph[*(lockedPairs+pairs).flatten].cycles.length > 0
+					p = pairs.sample
+					pairs.delete(p)
+					cyclic_pairs << p
+					results[:warnings] << p
+				end
+								
+				# Keep the acyclic pairs, discard the cylic ones
+				pairs.each { |p| results[:locking] << [p[0], p[1], "Kept"]; lockedPairs << p }
+				cyclic_pairs.each { |p| results[:locking] << [p[0], p[1], "Discarded"] }
+			end
+						
 			# Determine the winners and return the results
 			graph = RGL::DirectedAdjacencyGraph[*lockedPairs.flatten]
 			results[:winners] = results[:candidates] - graph.edges.collect { |e| e.target  }.uniq
